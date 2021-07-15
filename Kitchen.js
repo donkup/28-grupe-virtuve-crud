a9410fe4e503dff5ee4959d7d233439147163bd9
Donatas <donkuprys@gmail.com>
2021-07-15 19:50:52 +0300

    kitchen done

--- Kitchen.js
+++ Kitchen.js
@@ -1,7 +1,50 @@
 class Kitchen {
-    constructor() {
+    constructor(name) {
+        this.produktuSarasas = [];
+        this.name = name;
+    }
 
+    intro() {
+        console.log(`Sveiki atvyke i ${this.name} virtuve ;)`);
+    }
+
+    nupirkta(pavadinimas, kiekis) {
+        this.produktuSarasas.push({ pavadinimas, kiekis, nesugedes: true });
+    }
+
+    turimiProduktai() {
+        let turimiProduktai = 0;
+        for (let i = 0; i < this.produktuSarasas.length; i++) {
+            const produktas = this.produktuSarasas[i];
+            if (produktas.nesugedes) {
+                console.log(
+
+                    `${++turimiProduktai})${produktas.pavadinimas} : ${produktas.kiekis}`
+                );
+
+            }
+
+        }
+    }
+    sunaudota(pavadinimas, naujasKiekis) {
+        for (let i = 0; i < this.produktuSarasas.length; i++) {
+            const produktas = this.produktuSarasas[i];
+            if (produktas.pavadinimas === pavadinimas) {
+                produktas.kiekis -= naujasKiekis;
+            }
+
+        }
+    }
+    sugedesProduktas(pavadinimas) {
+        const naujasProduktuSarasas = [];
+        for (let i = 0; i < this.produktuSarasas.length; i++) {
+            const produktas = this.produktuSarasas[i];
+            if (produktas.pavadinimas !== pavadinimas) {
+                naujasProduktuSarasas.push(produktas)
+            }
+
+        }
+        this.produktuSarasas = naujasProduktuSarasas;
     }
 }
-
 module.exports = Kitchen;
